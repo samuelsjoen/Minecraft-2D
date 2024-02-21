de0eb0eb754bed92eacb03d2b67a8e29e6e9b6fe
samuel <samuelalois02@gmail.com>
2024-02-21 16:09:30 +0100

    Begynte på grafiske siden av inventory og items

--- core/src/com/mygdx/game/model/Inventory.java
+++ core/src/com/mygdx/game/model/Inventory.java
@@ -2,17 +2,47 @@ package com.mygdx.game.model;
 
 import java.util.HashMap;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.utils.Constants;
+
 public class Inventory {
     private HashMap<String, Integer> items;
     private int maxItems;
     private int currentItems;
+    private int currentSlot;
+    private Texture inventorySprite;
+    private Texture selectedItem;
+    private float xInv, yInv;
+    private float xItm, yItm;
+    private float invJump;
+    private float scale;
 
     public Inventory() {
         items = new HashMap<String, Integer>();
         maxItems = 10;
+        currentItems = items.size();
+        currentSlot = 0;
+
+        inventorySprite = new Texture(Gdx.files.internal("inventory.png"));
+        selectedItem = new Texture(Gdx.files.internal("selectedItem.png"));
+
+        // Coordinates for the inventory
+        xInv = 0;
+        yInv = 0;
+        
+        // Coordinates for the first item in inventory
+        xItm = xInv + 5;
+        yItm = yInv + 5;
+
+        // The amount of pixels to jump to the next item slot
+        invJump = 25;
+
+        this.scale = 1f;
+      
+        // Default items
         addItem(Item.SWORD.getName());
         addItem(Item.PICKAXE.getName());
-        currentItems = items.size();
     }
 
     public void addItem(String name, int quantity) {
@@ -22,7 +52,6 @@ public class Inventory {
             if (!isFull()) {
                 items.put(name, quantity);
             }
-            
         }
     }
 
@@ -49,5 +78,9 @@ public class Inventory {
 
     public boolean isFull() {
         return currentItems >= maxItems;
+    }
+
+    public void renderItem() {
+
     }
 }
--- core/src/com/mygdx/game/model/Item.java
+++ core/src/com/mygdx/game/model/Item.java
@@ -1,18 +1,20 @@
 package com.mygdx.game.model;
 
 public enum Item {
-    SWORD("Sword", "A sharp sword for fighting"),
-    PICKAXE("Pickaxe", "A pickaxe for mining"),
-    DIRT("Dirt", "A block of dirt"),
-    GRASS("Grass", "A block of grass"),
-    STONE("Stone", "A block of stone");
+    SWORD("Sword", "A sharp sword for fighting", "sword.png"),
+    PICKAXE("Pickaxe", "A pickaxe for mining", "pickaxe.png"),
+    DIRT("Dirt", "A block of dirt", "dirt.png"),
+    GRASS("Grass", "A block of grass", "grass.png"),
+    STONE("Stone", "A block of stone", "stone.png");
 
     private final String name;
     private final String description;
+    private final String texture;
 
-    Item(String name, String description) {
+    Item(String name, String description, String texture) {
         this.name = name;
         this.description = description;
+        this.texture = texture;
     }
 
     public String getName() {
