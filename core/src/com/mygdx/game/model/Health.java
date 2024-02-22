1dc2093e5feada8e0527e70a35ec1280b5c5afa4
samuel <samuelalois02@gmail.com>
2024-02-22 19:50:39 +0100

    small change in imports for health.java

--- core/src/com/mygdx/game/model/Health.java
+++ core/src/com/mygdx/game/model/Health.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.mygdx.game.utils.Constants;
 
 public class Health {
     private int health;
