40f45e93ed6a033d57f37d9aa09ea61de19afa70
martine.larsen <caj015@uib.no>
2024-03-18 23:57:30 +0100

    Combined healthview and inventoryview temporarily- think this will make it easier to improve the code to mvc-standards

--- core/src/com/minecraft/game/view/OverlayView.java
+++ core/src/com/minecraft/game/view/OverlayView.java
@@ -4,13 +4,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.minecraft.game.model.GameEntity;
+import com.minecraft.game.model.Health;
 import com.minecraft.game.model.Inventory;
 import com.minecraft.game.model.Item;
+import com.minecraft.game.model.Player;
 import com.minecraft.game.utils.Constants;
 
-public class InventoryView extends GameEntity {
+public class OverlayView extends GameEntity {
 
     private Texture inventorySprite;
     private Texture selectedItem;
@@ -19,7 +22,13 @@ public class InventoryView extends GameEntity {
     private Inventory inventory;
     private BitmapFont font;
 
-    public InventoryView(float width, float height, Body body, Inventory inventory) {
+    private Texture healthBarSheet;
+    private TextureRegion[][] splitFrames;
+    @SuppressWarnings("unused")
+    private Health health;
+    private float xHealth, yHealth;
+
+    public OverlayView(float width, float height, Body body, Inventory inventory, Health health) {
         super(width, height, body);
         this.inventory = inventory;
         this.inventorySprite = new Texture(Gdx.files.internal("assets/overlay/inventory.png"));
@@ -28,6 +37,11 @@ public class InventoryView extends GameEntity {
 
         // The amount of pixels to jump to the next item slot
         this.invJump = 40;
+
+        this.healthBarSheet = new Texture(Gdx.files.internal("assets/overlay/healthBar.png"));
+        this.splitFrames = TextureRegion.split(healthBarSheet, healthBarSheet.getWidth(),
+                healthBarSheet.getHeight() / 5);
+        this.health = health;
     }
 
     private void renderItems(SpriteBatch batch) {
@@ -45,6 +59,11 @@ public class InventoryView extends GameEntity {
         batch.draw(inventorySprite, x, y);
         renderCurrentSlot(batch);
         renderItems(batch);
+
+        int playerHealth = Player.getHealth().getHealth();
+        if (playerHealth > 0) {
+            batch.draw(splitFrames[playerHealth - 1][0], xHealth, yHealth, 200, 40);
+        }
     }
 
     private void renderCurrentSlot(SpriteBatch batch) {
@@ -57,5 +76,9 @@ public class InventoryView extends GameEntity {
         y = body.getPosition().y * Constants.PPM + 300;
         xItm = x + 5;
         yItm = y + 5;
+
+        xHealth = body.getPosition().x * Constants.PPM - 600;
+        yHealth = body.getPosition().y * Constants.PPM + 305;
+
     }
 }
