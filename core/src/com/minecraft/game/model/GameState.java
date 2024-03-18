19eeb63a199c26dfe48714d1adac21fe3a14cd92
martine.larsen <caj015@uib.no>
2024-03-18 23:59:48 +0100

    Added GameState enum to better handle the change between homescreen and gamescreen etc. Better overvier of different stages of the game/minecraft

---
+++ core/src/com/minecraft/game/model/GameState.java
@@ -0,0 +1,6 @@
+package com.minecraft.game.model;
+
+/** an enum which contains the possible GameState values */
+public enum GameState {
+    WELCOME_SCREEN, PAUSED_SCREEN, GAME_ACTIVE, GAME_OVER;
+} 
--- core/src/com/minecraft/game/model/MinecraftModel.java
+++ core/src/com/minecraft/game/model/MinecraftModel.java
@@ -1,12 +1,35 @@
 package com.minecraft.game.model;
 
+import com.minecraft.game.controller.ControllableMinecraftModel;
 import com.minecraft.game.model.entities.EntityFactory;
 import com.minecraft.game.utils.MinecraftMap;
+import com.minecraft.game.view.ViewableMinecraftModel;
 
-public class MinecraftModel {
+public class MinecraftModel implements ViewableMinecraftModel, ControllableMinecraftModel {
+
+    @SuppressWarnings("unused")
+    private MinecraftMap map;
+    @SuppressWarnings("unused")
+    private EntityFactory factory;
+
+    private GameState gameState;
 
     public MinecraftModel(MinecraftMap map, EntityFactory factory) {
-        //TODO Auto-generated constructor stub
+        this.map = map;
+        this.factory = factory;
+
+        this.gameState = GameState.GAME_ACTIVE; 
+        //this.gameState = GameState.WELCOME_SCREEN;
+    }
+
+    @Override
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    @Override
+    public void setGameState(GameState state) {
+        gameState = state;
     }
 
 }
