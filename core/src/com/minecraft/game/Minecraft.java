a248c53c91a112541468fb9aa28d0639bc6e0711
martine.larsen <caj015@uib.no>
2024-03-19 00:02:05 +0100

    Removed unused imports and changed some parameters and added that controller is the inputprocessor of the game.

--- core/src/com/minecraft/game/Minecraft.java
+++ core/src/com/minecraft/game/Minecraft.java
@@ -15,9 +15,6 @@ public class Minecraft extends Game {
 	public static Minecraft INSTANCE;
 	private int widthScreen, heightScreen;
 	public OrthographicCamera camera;
-
-    private MinecraftView view;
-
 	
 	public Minecraft() {
 		INSTANCE = this;
@@ -34,8 +31,10 @@ public class Minecraft extends Game {
 		EntityFactory factory = new EntityFactory();
 		MinecraftMap map = new MinecraftMap();
 		MinecraftModel model = new MinecraftModel(map, factory);
-		MinecraftView view = new MinecraftView(this);
-		new MinecraftController(model,view);
+		MinecraftView view = new MinecraftView(this, model);
+		MinecraftController controller = new MinecraftController(model,view);
+
+		Gdx.input.setInputProcessor(controller); // comment this out to make WorldController "work" placing tiles etc. I don't think we can have multiple input processors
 
 	}
 
@@ -48,6 +47,5 @@ public class Minecraft extends Game {
     @Override
     public void dispose() {
         // Dispose of resources when the game is closing
-        view.dispose();
     }
 }
