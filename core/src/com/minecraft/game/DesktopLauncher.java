03ec9a506139fdaebfb61171f1195c989c51c4c6
martine.larsen <caj015@uib.no>
2024-04-01 15:45:29 +0200

    Fixed the -xstartonfirstthread error for mac

--- core/src/com/minecraft/game/DesktopLauncher.java
+++ core/src/com/minecraft/game/DesktopLauncher.java
@@ -4,9 +4,6 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.minecraft.game.utils.Constants;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
-// /usr/bin/env /usr/local/Cellar/openjdk@17/17.0.5/libexec/openjdk.jdk/Contents/Home/bin/java -XstartOnFirstThread @/var/folders/z7/tfjly_9s08z85qmpz7p1vwlc0000gn/T/cp_acpu49xneo737uq9e5khdlkds.argfile com.minecraft.game.DesktopLauncher
-
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
